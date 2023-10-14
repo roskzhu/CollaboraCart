67815b8e8fe68b45e960d481643bbbfb15d2640a
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-10-13 23:47:07 -0400

    Add font

--- frontend/src/pages/landing.js
+++ frontend/src/pages/landing.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Landing = () => {
   return (
-    <div className="bg-white h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl text-black font-semibold mb-4">CollaboraCart</h1>
+    <div className="bg-white h-screen flex flex-col justify-center items-center font-inter">
+      <h1 className="text-3xl text-black font-bold mb-4">CollaboraCart</h1>
       <p className="text-black text-lg">Welcome to CollaboraCart!</p>
       <div className="mt-8">
         <button className="bg-[#0084fc] hover:bg-[#4168bb] text-white font-semibold py-2 px-4 rounded-full">
