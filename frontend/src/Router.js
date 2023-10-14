15d73f326b9603eb3dfa1e29016b25803867f5c8
roskzhu <roskenzhu@gmail.com>
2023-10-14 02:38:52 -0400

    added sign up form for businesses

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./pages/landing";
+import BusinessForm from "./pages/BusinessForm";
+// import { Landing, BusinessForm } from "./pages";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/BusinessSignUp" element={<BusinessForm />} />
       </Routes>
     </Router>
   );
--- frontend/src/pages/landing.js
+++ frontend/src/pages/landing.js
@@ -1,4 +1,6 @@
 import React from "react";
+import BusinessForm from "./BusinessForm";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -6,9 +8,11 @@ const Landing = () => {
       <h1 className="text-3xl text-black font-bold mb-4">CollaboraCart</h1>
       <p className="text-black text-lg">Welcome to CollaboraCart!</p>
       <div className="mt-8">
+        <Link to="/BusinessSignUp">
         <button className="bg-[#0084fc] hover:bg-[#4168bb] text-white font-semibold py-2 px-4 rounded-full">
           Get Started
         </button>
+        </Link>
       </div>
     </div>
   );
