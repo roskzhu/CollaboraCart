b282a16e91fe31e5d5e478de3bb2496782c229e5
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-10-13 23:39:02 -0400

    Setup app routes

---
+++ frontend/src/pages/landing.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="bg-white h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl text-black font-semibold mb-4">CollaboraCart</h1>
+      <p className="text-black text-lg">Welcome to CollaboraCart!</p>
+      <div className="mt-8">
+        <button className="bg-[#0084fc] hover:bg-[#4168bb] text-white font-semibold py-2 px-4 rounded-full">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
