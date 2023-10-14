08237e635e5a89a1700c82087ac91ddcda0e7add
roskzhu <roskenzhu@gmail.com>
2023-10-14 03:42:38 -0400

    configured database to business form submission

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
+CORS(app)  
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# Database setup
+conn = sqlite3.connect('businesses.db')  # database is businesses.db, table is submissions
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS submissions 
+             (company_name text, business_sector text, location text, email text, employee_count int)''')
+conn.commit()
+conn.close()
+
+
+@app.route('/BusinessSignUp', methods=['POST'])
+def submit_form():
+    data = request.get_json()
+    app.logger.info(data)
+
+    company_name = data['company_name']
+    business_sector = data['business_sector']
+    location = data['location']
+    email = data['email']
+    employee_count = data['employee_count']
+
+    conn = sqlite3.connect('businesses.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO submissions VALUES (?, ?, ?, ?, ?)', (company_name, business_sector, location, email, employee_count))
+    conn.commit()
+    conn.close()
+
+    response = {'message': 'Form submitted successfully'}
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
