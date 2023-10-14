758bdba4222ef7540cd5f5fa6973a92f141df8fe
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-10-14 18:53:18 -0400

    Add loading in animation to landing

--- frontend/src/pages/landing.js
+++ frontend/src/pages/landing.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "../components/hero";
 import NavBar from "../components/ui/navbar";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Landing = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
   return (
     <>
-      <NavBar />
-      <Hero />
+      <div data-aos="fade-up" data-aos-duration="1000">
+        <NavBar />
+        <Hero />
+      </div>
     </>
   );
 };
