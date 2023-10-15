9e6370c41b3fcebde3d203b9b1d8707a38ba8208
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-10-14 23:15:26 -0400

    Fix annoying warning >:(

--- frontend/src/components/dashboard/WelcomeBanner.js
+++ frontend/src/components/dashboard/WelcomeBanner.js
@@ -32,7 +32,7 @@ function WelcomeBanner() {
   return (
     <div className="bg-gradient-to-r from-blue-200 to-purple-100 p-6 sm:p-6 rounded-2xl overflow-hidden mb-8 max-w-6xl mx-auto">
       <h1 className="text-xl md:text-3xl text-slate-800 font-bold mb-1">
-        {`${getGreeting()}, ${companyName}. 👋`}
+        {`${getGreeting()}, ${companyName} 👋`}
       </h1>
       <p className="text-slate-700">
         Here is what’s happening with your business today:
