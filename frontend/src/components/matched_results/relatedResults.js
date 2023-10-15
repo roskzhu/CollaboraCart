17770e91b48a6cd9ee8511ca6d6d91dea09cb76f
Karen Lin <karecare1234@gmail.com>
2023-10-15 08:40:57 -0400

    Added AI that helps generate related items

---
+++ frontend/src/components/matched_results/relatedResults.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function relatedResults() {
+
+    return(
+        <div className="flex-initial px-10 py-5 rounded-lg shadow-md container-sm mx-auto mb-20">
+
+            helelaksjgd
+
+
+        </div>
+    )
+
+
+
+
+}
+export default relatedResults;
